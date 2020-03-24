4b0c8d4046b1978a96f46f5e0d4793019be24df5
Rediet8abere <redietaberegesesse@gmail.com>
2020-03-24 09:41:00 -0700

    adding white node

--- laterProject/laterProject/wsgi.py
+++ laterProject/laterProject/wsgi.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 
 import os
 
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'laterProject.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root='/path/to/static/files')
+application.add_files('/path/to/more/static/files', prefix='more-files/')
