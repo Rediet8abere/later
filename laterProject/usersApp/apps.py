9616fb8ddbf3f154afb078066052de807f0bffb5
sandycamilo <scam499@lasierra.edu>
2020-03-27 12:40:25 -0700

    js slides

--- laterProject/usersApp/apps.py
+++ laterProject/usersApp/apps.py
@@ -1,8 +1,8 @@
-from django.apps import AppConfig
+# from django.apps import AppConfig
 
 
-class UsersappConfig(AppConfig):
-    name = 'usersApp'
+# class UsersappConfig(AppConfig):
+#     name = 'usersApp'
 
-    def ready(self):
-        import users.signals
+#     def ready(self):
+#         import users.signals
