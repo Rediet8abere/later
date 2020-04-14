81e80b935ce4b6843c67ee84d515b81a43902721
sandycamilo <scam499@lasierra.edu>
2020-04-13 20:48:38 -0700

    testing

--- laterProject/listApp/urls.py
+++ laterProject/listApp/urls.py
@@ -12,11 +12,7 @@ from . import views
 # from django.contrib.auth import views as auth_views
 # from django.conf import settings
 # from django.conf.urls.static import static
-<<<<<<< HEAD
-# from usersApp import views as user_views
-=======
 # from users import views as user_views
->>>>>>> sandy-camilo
 
 urlpatterns = [
     path('', BooksListView.as_view(), name='book-home'),
@@ -27,19 +23,10 @@ urlpatterns = [
 
     # path('register/', user_views.register, name="register"),
     # path('profile/', user_views.profile, name='profile'),
-<<<<<<< HEAD
-    # path('login/', auth_views.LoginView.as_view(template_name="usersApp/login.html"), name="login"),
-    # path('logout/', auth_views.LogoutView.as_view(template_name="usersApp/logout.html"), name="logout"),
-    # path('password-reset/', auth_views.PasswordResetView.as_view(template_name="usersApp/password_reset.html"), name="password_reset"),
-    # path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name="usersApp/password_reset_done.html"), name="password_reset_done"),
-    # path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="usersApp/password_reset_confirm.html"), name="password_reset_confirm"),
-    # path('', include('listApp.urls')),
-=======
     # path('login/', auth_views.LoginView.as_view(template_name="users/login.html"), name="login"),
     # path('logout/', auth_views.LogoutView.as_view(template_name="users/logout.html"), name="logout"),
     # path('password-reset/', auth_views.PasswordResetView.as_view(template_name="users/password_reset.html"), name="password_reset"),
     # path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name="users/password_reset_done.html"), name="password_reset_done"),
     # path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="users/password_reset_confirm.html"), name="password_reset_confirm"),
     # path('', include('blog.urls')),
->>>>>>> sandy-camilo
 ]
