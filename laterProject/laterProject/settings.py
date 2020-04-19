ee4391b22136d2473c39d174997c5293c220f865
sandycamilo <39415553+sandycamilo@users.noreply.github.com>
2020-04-19 02:56:13 -0700

    user registration

--- laterProject/laterProject/settings.py
+++ laterProject/laterProject/settings.py
@@ -33,13 +33,13 @@ ALLOWED_HOSTS = ['127.0.0.1', 'laterproject.herokuapp.com']
 INSTALLED_APPS = [
     'rest_framework',
     'listApp.apps.ListappConfig',
+    'userAccounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'userAccounts'
 ]
 
 MIDDLEWARE = [
